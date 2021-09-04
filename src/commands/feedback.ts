3df67a4755118ac7557c09217cfbfdadf099d4b5
AsgereDreemurr <36601572+AsgereDreemurr@users.noreply.github.com>
2021-09-04 23:56:44 +0200

    Fix indentation

--- src/commands/feedback.ts
+++ src/commands/feedback.ts
@@ -34,53 +34,53 @@ export default class Feedback extends Command {
     if (!ghCli.isAuthenticated()) return;
 
     await inquirer
-    .prompt([
-      {
-        name: "type",
-        message: "What type of feedback are you reporting?",
-        type: "list",
-        choices: [
-          { name: "bug", value: "bug" },
-          {
-            name: "enhancement (new feature or request)",
-            value: "enhancement",
-          },
-          {
-            name: "documentation (improvements or additions)",
-            value: "documentation",
-          },
-          { name: "question" },
-          { name: "other" },
-        ],
-      },
-      { name: "title", message: "Enter title:", type: "input" },
-      { name: "description", message: "Enter description:", type: "input" },
-      { name: "confirm", message: "Submit issue?", type: "confirm" },
-    ])
-    .then(function (answers) {
-      const { type, title, description, confirm } = answers;
+      .prompt([
+        {
+          name: "type",
+          message: "What type of feedback are you reporting?",
+          type: "list",
+          choices: [
+            { name: "bug", value: "bug" },
+            {
+              name: "enhancement (new feature or request)",
+              value: "enhancement",
+            },
+            {
+              name: "documentation (improvements or additions)",
+              value: "documentation",
+            },
+            { name: "question" },
+            { name: "other" },
+          ],
+        },
+        { name: "title", message: "Enter title:", type: "input" },
+        { name: "description", message: "Enter description:", type: "input" },
+        { name: "confirm", message: "Submit issue?", type: "confirm" },
+      ])
+      .then(function (answers) {
+        const { type, title, description, confirm } = answers;
 
-      if (confirm) {
-        const issue = shell.exec(
-          `gh issue create --title "${title}" --body "${description}" --label "${type}" --repo github.com/AsgereDreemurr/CLI-ITU"`,
-          { async: false, silent: true }
-        );
-        if (issue.stderr !== "") {
-          console.error(chalk.red(issue.stderr.trim()));
-          return;
+        if (confirm) {
+          const issue = shell.exec(
+            `gh issue create --title "${title}" --body "${description}" --label "${type}" --repo github.com/AsgereDreemurr/CLI-ITU"`,
+            { async: false, silent: true }
+          );
+          if (issue.stderr !== "") {
+            console.error(chalk.red(issue.stderr.trim()));
+            return;
+          }
+          console.log("✔ issue submitted:", issue.stdout.trim());
+          console.log("Thanks for the feedback!");
         }
-        console.log("✔ issue submitted:", issue.stdout.trim());
-        console.log("Thanks for the feedback!");
-      }
-    })
-    .catch(function (error) {
-      if (error.isTtyError) {
-        console.error(
-          "The prompt couldn't be rendered in the current environment"
-        );
-      } else {
-        console.error("Something went wrong while showing the issue prompt");
-      }
-    });
+      })
+      .catch(function (error) {
+        if (error.isTtyError) {
+          console.error(
+            "The prompt couldn't be rendered in the current environment"
+          );
+        } else {
+          console.error("Something went wrong while showing the issue prompt");
+        }
+      });
   }
 }
